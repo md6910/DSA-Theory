808b48247cc3c5fdbf558892bfdb0bc73495e320
md6910 <maazsid14@gmail.com>
2025-02-02 15:01:33 +0500

    queueLL.java

--- Queue/queueLL.java
+++ Queue/queueLL.java
@@ -1,17 +1,71 @@
-
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class queueLL {
-    public static void main(String[] args) {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
 
-        Queue<Integer> example=new LinkedList<>();
-        example.add(10);
-        example.add(20);
-        example.add(30);
 
-        System.out.println(""+example );
-        System.out.println(""+example.poll());
-        System.out.println("Queue after removal: "+example);
+    static class Queue {
+        static Node head = null;
+        static Node tail = null;
+
+
+        public static boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+
+        public static void add(int data) {
+            Node newNode = new Node(data);
+            if(isEmpty()) {
+                tail = head = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+
+        public static int remove() {
+            if(isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            int front = head.data;
+            //single node
+            if(head == tail) {
+                tail = null;
+            }
+            head = head.next;
+            return front;
+        }
+
+
+        public static int peek() {
+            if(isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+    public static void main(String args[]) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+
+        while(!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
     }
 }
