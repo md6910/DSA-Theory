b31e5c2ff1eac41d75bee71071da99b82b0696de
md6910 <maazsid14@gmail.com>
2025-02-02 15:00:47 +0500

    queuearray.java

--- Queue/queue.java
+++ Queue/queue.java
@@ -1,16 +1,59 @@
+public class queuearray {
+    static class Queue {
+        int arr[];
+        int size;
+        int front;
+        int rear;
 
-import java.util.Queue;
-import java.util.ArrayDeque;
-public class queue{
-    public static void main(String[] args) {
-        Queue<Integer> array=new ArrayDeque<>();
+        Queue(int size) {
+            this.size = size;
+            arr = new int[size];
+            front = 0;
+            rear = -1;
+        }
 
-        array.add(10);
-        array.add(20);
-        array.add(30);
+        public boolean isEmpty() {
+            return rear < front;
+        }
 
-        System.out.println(""+array);
-        System.out.println(""+array.poll());
-        System.out.println("Queue after Changed"+array);
+        public boolean isFull() {
+            return rear == size - 1;
+        }
+
+        public void add(int data) {
+            if (isFull()) {
+                System.out.println("Overflow");
+                return;
+            }
+            arr[++rear] = data;
+        }
+
+        // O(1) for remove operation
+        public int remove() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[front++];
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String args[]) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        System.out.println(q.peek());
+        System.out.println(q.remove());  // Should print 1
+        System.out.println(q.remove());  // Should print 2
+        System.out.println(q.remove());  // Should print 3
     }
 }
